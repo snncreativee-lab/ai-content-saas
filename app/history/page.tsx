e1744780621a2cf30f37b869eb572d4c2342833a
Syafiq Nadzri <snncreativee@gmail.com>
2026-06-24 11:25:55 +0800

    fix history page

---
+++ app/history/page.tsx
@@ -0,0 +1,8 @@
+export default function HistoryPage() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1 className="text-4xl font-bold">History Content</h1>
+      <p className="mt-4">History content dipaparkan dalam dashboard.</p>
+    </main>
+  );
+}
